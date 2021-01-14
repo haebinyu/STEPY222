aee5fb56853a3d3ef659899421957316b1dc22e7
goqls96 <alsrblove02@naver.com>
2021-01-14 13:47:57 +0900

    검색 페이지 이동 추가

--- src/main/java/com/bob/stepy/SearchController.java
+++ src/main/java/com/bob/stepy/SearchController.java
@@ -1,5 +1,20 @@
 package com.bob.stepy;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.java.Log;
+
+@Log
+@Controller
 public class SearchController {
+	
+	// 검색 페이지 이동
+	@GetMapping("searchFrm")
+	public String searchFrm() {
+		log.info("searchFrm()");
+		
+		return "searchFrm";
+	}
 
 }
